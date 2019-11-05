729bb907282e472f09fd37cfdf847ec450a577ea
Harrison Levin <harrison483@gmail.com>
2019-11-05 09:17:58 -0700

    recipe test half way built

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -1,8 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
+require './lib/recipe'
 
 class RecipeTest < Minitest::Test
 
+  def setup
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @mac_and_cheese
+  end
+
+  def test_variables
+    assert_equal "Mac and Cheese", @mac_and_cheese.name
+    assert_equal ([{}]), @mac_and_cheese.ingredients_required
+  end
+
+  def test_add_ingredient
+  end
+
+  def test_amount_required
+  end
 
 end
